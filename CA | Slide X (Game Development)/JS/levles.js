9f438a7cbd35d8f3c17976704079f383b975ebac
Sabari Narayana <sabariokg@gmail.com>
2023-09-08 12:37:23 +0530

    Levels for mobie is completed

--- CA | Slide X (Game Development)/JS/levles.js
+++ CA | Slide X (Game Development)/JS/levles.js
@@ -1,42 +1,97 @@
-// Form submission event
-const gamePointForm = document.getElementById("game_point_reader");
-gamePointForm.addEventListener("submit", function (e) {
-  e.preventDefault(); // Prevent the form from submitting and refreshing the page
+// Check if the window width is less than 768 pixels (mobile view)
+if (window.innerWidth < 768) {
+  // Hide the game point input and submit button initially
+  document.getElementById("game_point").style.display = "none";
+  document.querySelector("#button").style.display = "none";
 
-  // Get the game point input value
-  const gamePoint = document.getElementById("max_point").value;
-
-  // Store the data in local storage
-  localStorage.setItem("game_point", gamePoint);
-
-  // Redirect to the Game Page
-  window.location.href = "game.html";
-});
-
-// Checking for the selected game level
-const levelElements = document.querySelectorAll(".levels");
-levelElements.forEach((element) => {
-  element.addEventListener("click", handleLevelSelection);
-});
-
-// Function to handle level selection
-function handleLevelSelection(event) {
+  // Get all elements with the class "levels"
   const levelElements = document.querySelectorAll(".levels");
 
-  // Remove the "selected" class from all level elements and add the default classes
+  // Add click event listeners to each "level" element
   levelElements.forEach((element) => {
-    element.classList.remove("selected", "border_color");
-    element.classList.add("text_bgcolor", "text_color");
+    element.addEventListener("click", handleLevelSelection);
   });
 
-  // Add the "selected" class and border_color class to the clicked level
-  const selectedLevel = event.target;
-  selectedLevel.classList.add("selected", "border_color");
-  selectedLevel.classList.remove("text_bgcolor", "text_color");
+  // Function to handle level selection for mobile view
+  function handleLevelSelection(event) {
+    // Get the clicked level element
+    const selectedLevel = event.target;
 
-  // Get the selected level's text
-  const selectedLevelText = selectedLevel.textContent;
+    // Get the text content of the selected level
+    const selectedLevelText = selectedLevel.textContent;
 
-  // Store the selected level in local storage
-  localStorage.setItem("gameLevel", selectedLevelText);
+    // Store the selected level in local storage for later use
+    localStorage.setItem("gameLevel", selectedLevelText);
+
+    // Display the game point input and submit button
+    document.getElementById("game_point").style.display = "block";
+    document.querySelector("#button").style.display = "flex";
+
+    // Hide the level selector
+    document.querySelector("#level_selector").style.display = "none";
+
+    // Add a submit event listener to the game point form
+    const gamePointForm = document.getElementById("game_point_reader");
+    gamePointForm.addEventListener("submit", function (e) {
+      e.preventDefault(); // Prevent the form from submitting and refreshing the page
+
+      // Get the game point input value
+      const gamePoint = document.getElementById("max_point").value;
+
+      // Store the game point data in local storage
+      localStorage.setItem("game_point", gamePoint);
+
+      // Redirect to the Game Page
+      window.location.href = "game.html";
+    });
+  }
+} else {
+  // Desktop view handling
+
+  // Add a submit event listener to the game point form for desktop
+  const gamePointForm = document.getElementById("game_point_reader");
+  gamePointForm.addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent the form from submitting and refreshing the page
+
+    // Get the game point input value
+    const gamePoint = document.getElementById("max_point").value;
+
+    // Store the game point data in local storage
+    localStorage.setItem("game_point", gamePoint);
+
+    // Redirect to the Game Page
+    window.location.href = "game.html";
+  });
+
+  // Get all elements with the class "levels"
+  const levelElements = document.querySelectorAll(".levels");
+
+  // Add click event listeners to each "level" element for desktop
+  levelElements.forEach((element) => {
+    element.addEventListener("click", handleLevelSelection);
+  });
+
+  // Function to handle level selection for desktop view
+  function handleLevelSelection(event) {
+    const levelElements = document.querySelectorAll(".levels");
+
+    // Remove the "selected" class from all level elements and add default classes
+    levelElements.forEach((element) => {
+      element.classList.remove("selected", "border_color");
+      element.classList.add("text_bgcolor", "text_color");
+    });
+
+    // Get the clicked level element
+    const selectedLevel = event.target;
+
+    // Add the "selected" class and border_color class to the clicked level
+    selectedLevel.classList.add("selected", "border_color");
+    selectedLevel.classList.remove("text_bgcolor", "text_color");
+
+    // Get the selected level's text
+    const selectedLevelText = selectedLevel.textContent;
+
+    // Store the selected level in local storage for later use
+    localStorage.setItem("gameLevel", selectedLevelText);
+  }
 }
--- CA | Slide X (Game Development)/JS/player_info.js
+++ CA | Slide X (Game Development)/JS/player_info.js
@@ -1,22 +1,92 @@
-// Add an event listener to the form submission
-document
-  .getElementById("player_details")
-  .addEventListener("submit", function (e) {
-    // Prevent the form from submitting and refreshing the page
+// Get references to the form and player sections
+const playerDetailsForm = document.getElementById("player_details");
+const player1Section = document.getElementById("player1");
+const player2Section = document.getElementById("player2");
+
+// Check if the window width is less than 768px
+if (window.innerWidth < 768) {
+  // Hide the initial submit button
+  document.querySelector("#player_details > #button").style.display = "none";
+
+  // Add a click event listener to the "Next" button
+  document.querySelector(".next").addEventListener("click", function () {
+    // Get values from Player 1 input fields
+    const player1Name = document.getElementById("player1-name").value;
+    const player1Nickname = document.getElementById("player1-nickname").value;
+
+    // Check if Player 1 fields are empty
+    if (!player1Name || !player1Nickname) {
+      alert("Please fill in all Player 1 fields.");
+      return;
+    }
+
+    // Store Player 1 data in local storage
+    localStorage.setItem("player1_name", player1Name);
+    localStorage.setItem("player1_nickname", player1Nickname);
+
+    // Hide "Next" button and show the submit button
+    document.querySelector(".next").style.display = "none";
+    document.querySelector("#player_details > #button").style.display = "flex";
+
+    // Hide Player 1 section and show Player 2 section
+    player1Section.style.display = "none";
+    player2Section.style.display = "block";
+
+    // Prevent form submission on "Next" button click
+    document
+      .getElementById("player_details")
+      .addEventListener("submit", function (e) {
+        e.preventDefault();
+      });
+  });
+
+  // Add a submit event listener to the form
+  document
+    .getElementById("player_details")
+    .addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      // Get values from Player 2 input fields
+      const player2Name = document.getElementById("player2-name").value;
+      const player2Nickname = document.getElementById("player2-nickname").value;
+
+      // Check if Player 2 fields are empty
+      if (!player2Name || !player2Nickname) {
+        alert("Please fill in all Player 2 fields.");
+        return;
+      }
+
+      // Store Player 2 data in local storage
+      localStorage.setItem("player2_name", player2Name);
+      localStorage.setItem("player2_nickname", player2Nickname);
+
+      // Redirect to the "leves.html" page
+      window.location.href = "leves.html";
+    });
+} else {
+  // Add a submit event listener to the form for larger screens
+  playerDetailsForm.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    // Get values from the form
+    // Get values from all input fields
     const player1Name = document.getElementById("player1-name").value;
     const player1Nickname = document.getElementById("player1-nickname").value;
     const player2Name = document.getElementById("player2-name").value;
     const player2Nickname = document.getElementById("player2-nickname").value;
 
-    // Store the collected data in local storage
+    // Check if any of the fields are empty
+    if (!player1Name || !player1Nickname || !player2Name || !player2Nickname) {
+      alert("Please fill in all fields.");
+      return;
+    }
+
+    // Store all collected data in local storage
     localStorage.setItem("player1_name", player1Name);
     localStorage.setItem("player2_name", player2Name);
     localStorage.setItem("player1_nickname", player1Nickname);
     localStorage.setItem("player2_nickname", player2Nickname);
 
-    // redirecting to Game level Page
+    // Redirect to the "leves.html" page
     window.location.href = "leves.html";
   });
+}
