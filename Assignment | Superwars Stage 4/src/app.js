8b78c8856cc05e7cda9f63ca81b70d4771530a77
Sabari Narayana <sabariokg@gmail.com>
2023-09-25 11:09:13 +0530

    Superwars stage 4 complected

--- Assignment | Superwars Stage 4/src/app.js
+++ Assignment | Superwars Stage 4/src/app.js
@@ -1,7 +1,7 @@
 const PLAYERS = [
-  'Spiderman',
-  'Captain America',
-  'Wonderwoman',
+  "Spiderman",
+  "Captain America",
+  "Wonderwoman",
   // "Popcorn",
   // "Gemwoman",
   // "Bolt",
@@ -25,6 +25,11 @@ const PLAYERS = [
 class Player {
   constructor(id, name, type) {
     // Progression 1: Create member variables and assign values
+    this.id = id;
+    this.name = name;
+    this.strength = this.getRandomStrength();
+    this.image = `images/super-${id + 1}.png`;
+    this.type = type;
   }
   // getting random strength
   getRandomStrength = () => {
@@ -35,7 +40,20 @@ class Player {
   view = () => {
     // Accumulate HTML template
     // Type your code here
-
+    let player = document.createElement("div");
+    player.classList.add("player");
+    player.setAttribute("data-id", this.id);
+    let img = document.createElement("img");
+    img.src = this.image;
+    player.append(img);
+    let name = document.createElement("div");
+    name.classList.add("name");
+    name.innerText = this.name;
+    player.append(name);
+    let strength = document.createElement("div");
+    strength.classList.add("strength");
+    strength.innerText = this.strength;
+    player.append(strength);
     return player;
   };
 }
@@ -47,18 +65,22 @@ class Superwar {
     // Create a field players
     // Use Map method to loop through players argument and create new players
     // Type your code here
+    this.players = players.map((playerName, index) => {
+      const playerType = index % 2 === 0 ? "hero" : "villain";
+      return new Player(index, playerName, playerType);
+    });
   }
 
   // Display players in HTML
   viewPlayers = () => {
-    let team = document.getElementById('heroes');
-    team.innerHTML = '';
-    let fragment = this.buildPlayers('hero');
+    let team = document.getElementById("heroes");
+    team.innerHTML = "";
+    let fragment = this.buildPlayers("hero");
     team.append(fragment);
 
-    team = document.getElementById('villains');
-    team.innerHTML = '';
-    fragment = this.buildPlayers('villain');
+    team = document.getElementById("villains");
+    team.innerHTML = "";
+    fragment = this.buildPlayers("villain");
     team.append(fragment);
   };
 
@@ -73,7 +95,7 @@ class Superwar {
 }
 
 // uncomment this part -- only after you complete progression 3
-// window.onload = () => {
-//     const superwar = new Superwar(PLAYERS);
-//     superwar.viewPlayers();
-// }
+window.onload = () => {
+  const superwar = new Superwar(PLAYERS);
+  superwar.viewPlayers();
+};
