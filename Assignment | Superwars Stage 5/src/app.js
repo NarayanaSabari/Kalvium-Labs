9af39f02df7fc30bfcb5adc01e1277c69a2d0fed
Sabari Narayana <sabariokg@gmail.com>
2023-09-25 13:09:51 +0530

    index.html updated

--- Assignment | Superwars Stage 5/src/app.js
+++ Assignment | Superwars Stage 5/src/app.js
@@ -1,24 +1,24 @@
 const PLAYERS = [
-  'Spiderman',
-  'Captain America',
-  'Wonderwoman',
-  'Popcorn',
-  'Gemwoman',
-  'Bolt',
-  'Antwoman',
-  'Mask',
-  'Tiger',
-  'Captain',
-  'Catwoman',
-  'Fish',
-  'Hulk',
-  'Ninja',
-  'Black Cat',
-  'Volverine',
-  'Thor',
-  'Slayer',
-  'Vader',
-  'Slingo',
+  "Spiderman",
+  "Captain America",
+  "Wonderwoman",
+  "Popcorn",
+  "Gemwoman",
+  "Bolt",
+  "Antwoman",
+  "Mask",
+  "Tiger",
+  "Captain",
+  "Catwoman",
+  "Fish",
+  "Hulk",
+  "Ninja",
+  "Black Cat",
+  "Volverine",
+  "Thor",
+  "Slayer",
+  "Vader",
+  "Slingo",
 ];
 
 // Player Class
@@ -26,7 +26,7 @@ class Player {
   constructor(id, name, type) {
     this.id = id;
     this.name = name;
-    this.image = 'images/super-' + (id + 1) + '.png';
+    this.image = "images/super-" + (id + 1) + ".png";
     this.strength = this.getRandomStrength();
     this.type = type;
     this.selected = false;
@@ -40,17 +40,17 @@ class Player {
 
   // Create a player for displaying
   view = () => {
-    let player = document.createElement('div');
-    player.classList.add('player');
-    player.setAttribute('data-id', this.id);
-    if (this.selected == true) player.classList.add('selected');
-    let image = document.createElement('img');
-    image.setAttribute('src', this.image);
-    let name = document.createElement('div');
+    let player = document.createElement("div");
+    player.classList.add("player");
+    player.setAttribute("data-id", this.id);
+    if (this.selected == true) player.classList.add("selected");
+    let image = document.createElement("img");
+    image.setAttribute("src", this.image);
+    let name = document.createElement("div");
     name.textContent = this.name;
-    let strength = document.createElement('div');
+    let strength = document.createElement("div");
     strength.textContent = this.strength;
-    strength.className = 'strength';
+    strength.className = "strength";
     player.append(image, name, strength);
     return player;
   };
@@ -60,12 +60,12 @@ class Player {
 class Superwar {
   constructor(players) {
     this.players = players.map((player, i) => {
-      let type = i % 2 == 0 ? 'hero' : 'villain';
+      let type = i % 2 == 0 ? "hero" : "villain";
       return new Player(i, player, type);
     });
     this.score = [0, 0];
-    Array.from(document.getElementsByClassName('team')).forEach((elem) =>
-      elem.addEventListener('click', (e) => {
+    Array.from(document.getElementsByClassName("team")).forEach((elem) =>
+      elem.addEventListener("click", (e) => {
         this.handleSelection(e.target);
       })
     );
@@ -73,14 +73,14 @@ class Superwar {
 
   // Display players in HTML
   viewPlayers = () => {
-    let team = document.getElementById('heroes');
-    team.innerHTML = '';
-    let fragment = this.buildPlayers('hero');
+    let team = document.getElementById("heroes");
+    team.innerHTML = "";
+    let fragment = this.buildPlayers("hero");
     team.append(fragment);
 
-    team = document.getElementById('villains');
-    team.innerHTML = '';
-    fragment = this.buildPlayers('villain');
+    team = document.getElementById("villains");
+    team.innerHTML = "";
+    fragment = this.buildPlayers("villain");
     team.append(fragment);
   };
 
@@ -100,22 +100,27 @@ class Superwar {
 
   // Handle player clicks
   handleSelection = (target) => {
-    if (!target.classList.contains('player')) target = target.parentNode;
-    if (!target.hasAttribute('data-id')) return;
+    if (!target.classList.contains("player")) target = target.parentNode;
+    if (!target.hasAttribute("data-id")) return;
 
-    let selectedId = target.getAttribute('data-id');
+    let selectedId = target.getAttribute("data-id");
     let selectedPlayer = this.players[selectedId];
     this.players
       .filter((player) => player.type == selectedPlayer.type)
       .forEach((player) => (player.selected = false));
     selectedPlayer.selected = true;
 
-    if (this.isFight() === 'clash') this.fight();
+    if (this.isFight() === "clash") this.fight();
     else this.viewPlayers();
   };
 
   // Progression 1: Check for fight
-  isFight = () => {
+  isFight = (strength) => {
+    if (strength > 0) {
+      return "clash";
+    } else {
+      return "peace";
+    }
     // Type your code here
     // return  'clash' or 'peace';
   };
@@ -125,25 +130,62 @@ class Superwar {
     // Filtered the selected players and calculate score
     // Should return HTML element with score
     // Type your code here
+    let score = this.calculateScore();
 
-    if (this.checkWin() !== 'endure')
+    let finalScore = document.getElementById("score");
+    finalScore.innerHTML = "1 - 0";
+
+    if (this.checkWin() !== "endure")
       setTimeout(() => this.announceWinner(score), 100);
+
+    return finalScore;
   };
 
   // Progression 2: Calculate score
   calculateScore = () => {
+    let score = { hero: 0, villain: 0 };
     // Calculate and return the total score of teams
     // Type your code here
+    let setWinner = this.updateWin();
+    this.players.map((player) => {
+      if (player.type == "hero") {
+        player.win = setWinner["hero"];
+        score["hero"] += player.wins;
+      } else {
+        player.win = setWinner["villain"];
+        score["villain"] += player.wins;
+      }
+    });
 
     return score;
   };
+  updateWin = () => {
+    let win = { hero: 0, villain: 0, draw: 0 };
+    let winner = this.checkWin();
+    if (winner == "hero") {
+      win["hero"] += 1;
+    } else if (winner == "villain") {
+      win["villain"] += 1;
+    } else {
+      win["draw"] += 1;
+    }
 
+    return win;
+  };
   // Progression 3: Check whether there is a win
   checkWin = () => {
     // Find the winner if exists return type hero or villain
     // If winner dosen't exists then return endure
     // Type your code here
-
+    let result = "endure";
+    let hero_strength = this.totalStrength("hero");
+    let villain_strength = this.totalStrength("villain");
+    result =
+      hero_strength > villain_strength
+        ? "hero"
+        : hero_strength == villain_strength
+        ? "endure"
+        : "villain";
     return result;
   };
 
@@ -151,15 +193,27 @@ class Superwar {
   totalStrength = (type) => {
     // Calculate and return the total strength of the team
     // Type your code here
-
-    return strength;
+    let hero_strength = 0;
+    let villain_strength = 0;
+    this.players.map((player) => {
+      if (player.type == "hero") {
+        hero_strength += player.strength;
+      } else {
+        villain_strength += player.strength;
+      }
+    });
+    if (type === "hero") {
+      return hero_strength;
+    } else {
+      return villain_strength;
+    }
   };
 
   // Announce the winner
   announceWinner = (score) => {
-    if (score['hero'] == score['villain']) alert('Its a draw!');
-    else if (score['hero'] > score['villain']) alert('Heroes Win!');
-    else alert('Villains Win!');
+    if (score["hero"] == score["villain"]) alert("Its a draw!");
+    else if (score["hero"] > score["villain"]) alert("Heroes Win!");
+    else alert("Villains Win!");
     location.reload();
   };
 }
